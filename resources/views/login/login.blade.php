ffb5cb5bce13480161ee7b4f5ed2fc2c2c5dfdf7
inizaldi <rizaldiakbar57@gmail.com>
2022-11-28 13:40:30 +0700

    update tittle

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>C-Ad - Login</title>
+    <title>C-Archive - Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('templates/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
