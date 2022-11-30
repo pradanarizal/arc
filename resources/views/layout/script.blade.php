e2c74e6b611a09af46b4e29e3d269710043eecb3
Rifky Yusuf Mahfuz <rifkyyusufmahfud@gmail.com>
2022-11-30 18:37:39 +0700

    Script

--- resources/views/layout/script.blade.php
+++ resources/views/layout/script.blade.php
@@ -11,13 +11,13 @@
 <!-- Page level plugins -->
 <script src="{{ asset('templates/vendor/datatables/jquery.dataTables.min.js') }}"></script>
 <script src="{{ asset('templates/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
-<script src="{{ asset('templates/vendor/chart.js/Chart.min.js')}}"></script>
+<script src="{{ asset('templates/vendor/chart.js/Chart.min.js') }}"></script>
 
 <!-- Page level custom scripts -->
 <script src="{{ asset('templates/js/demo/datatables-demo.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
 </script>
-<script src="{{asset('templates/js/bar-chart-penyimpanan.js') }}"></script>
+<script src="{{ asset('templates/js/bar-chart-penyimpanan.js') }}"></script>
 
 {{-- CDN Sweet Alert --}}
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
@@ -28,3 +28,4 @@
 <link rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
+{{-- KHUSUS SCRIPT BARU TARO DI BAWAH INI --}}
