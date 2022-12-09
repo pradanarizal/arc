0232b91efe3fae2e56b3a57ac8b15c4d32b26dc4
dolyded <dolyprahoro@gmail.com>
2022-12-09 12:56:09 +0700

    fix error dokumen admin & superadmin,

--- app/Http/Controllers/admin/riwayat/RiwayatpengarsipanController.php
+++ app/Http/Controllers/admin/riwayat/RiwayatpengarsipanController.php
@@ -17,7 +17,7 @@ class RiwayatpengarsipanController extends Controller
     public function index()
     {
         $data = [
-            'dokumen' => $this->Model->getDataByDivisi(Auth::user()->divisi),
+            'dokumen' => $this->Model->getDataByDivisi(Auth::user()->id_departemen),
         ];
 
         return view('admin.riwayat.riwayat-pengarsipan', $data);
