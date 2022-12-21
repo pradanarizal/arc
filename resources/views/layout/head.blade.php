6e3c90d1c6951817a38e84dd580c757e28b3b92f
Than1st <1911500641@student.budiluhur.ac.id>
2022-12-21 11:44:32 +0700

    Input Year

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -18,3 +18,6 @@
 
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet">
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+{{-- custom --}}
+<link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/css/bootstrap-datepicker.css">
