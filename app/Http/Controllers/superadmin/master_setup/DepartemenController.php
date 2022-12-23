4884cdd09fa414cb1fd6d0b1126955ae90780e0e
dolyded <dolyprahoro@gmail.com>
2022-12-23 17:48:38 +0700

    update departemen

--- app/Http/Controllers/superadmin/master_setup/DepartemenController.php
+++ app/Http/Controllers/superadmin/master_setup/DepartemenController.php
@@ -74,26 +74,42 @@ class DepartemenController extends Controller
 
     public function update(Request $request, $id)
     {
-        $this->validate(
-            $request,
-            [
-                // 'kode_departemen' => 'required|unique:departemen,kode_departemen',
-                // 'nama_departemen' => 'required|unique:departemen,nama_departemen'
-                'kode_departemen' => 'required',
-                'nama_departemen' => 'required'
-            ],
-            [
-                'kode_departemen.required' => 'Kode Departemen wajib diisi!',
-                // 'kode_departemen.unique' => 'Kode Departemen sudah ada!',
-                'nama_departemen.required' => 'Nama Departemen wajib diisi!',
-                // 'nama_departemen.unique' => 'Nama Departemen sudah ada!'
-            ]
-        );
+        $data = [];
+        if($request->input('kode_departemen_edit') != $request->input('old_kode_departemen_edit')){
+            $this->validate($request,
+                [
+                    'kode_departemen_edit' => 'required|unique:departemen,kode_departemen',
+                    'nama_departemen_edit' => 'required'
+                ],
+                [
+                    'kode_departemen_edit.required' => 'Kode Departemen wajib diisi!',
+                    'kode_departemen_edit.unique' => 'Kode Departemen sudah ada!',
+                    'nama_departemen_edit.required' => 'Nama Departemen wajib diisi!',
+                ]
+            );
 
-        $data = [
-            'kode_departemen' => $request->input('kode_departemen'),
-            'nama_departemen' => $request->input('nama_departemen')
-        ];
+            $data = [
+                'kode_departemen' => $request->input('kode_departemen_edit'),
+                'nama_departemen' => $request->input('nama_departemen_edit')
+            ]; 
+
+        } else {
+            $this->validate($request,
+                [
+                    'kode_departemen_edit' => 'required',
+                    'nama_departemen_edit' => 'required'
+                ],
+                [
+                    'kode_departemen_edit.required' => 'Kode Departemen wajib diisi!',
+                    'nama_departemen_edit.required' => 'Nama Departemen wajib diisi!',
+                ]
+            );  
+        
+            $data = [
+                'kode_departemen' => $request->input('kode_departemen_edit'),
+                'nama_departemen' => $request->input('nama_departemen_edit')
+            ]; 
+        }
 
         if ($this->DepartemenModel->update_departemen($data, $id)) {
             return redirect('/master_setup/data_departemen')->with('toast_success', 'Berhasil Edit Departemen');
--- resources/views/superadmin/modal/m_edit_departemen.blade.php
+++ resources/views/superadmin/modal/m_edit_departemen.blade.php
@@ -17,13 +17,22 @@
                         @method('PUT')
                         <div class="form-group">
                             <label for="kode_departemen">Kode Departemen</label>
-                            <input type="text" value="{{$item->kode_departemen}}" class="form-control" id="kode_departemen" name="kode_departemen"
+                            <input type="text" value="{{$item->kode_departemen}}" class="form-control" id="kode_departemen" name="kode_departemen_edit"
                                 aria-describedby="emailHelp">
+                            <input type="text" name="old_kode_departemen_edit" class="form-control"
+                                value="{{ $item->kode_departemen }}" hidden>
+
+                            @error('kode_departemen_edit')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <div class="form-group">
-                            <label for="kode_departemen">Nama Departemen</label>
-                            <input type="text" value="{{$item->nama_departemen}}" class="form-control" id="nama_departemen" name="nama_departemen"
+                            <label for="nama_departemen">Nama Departemen</label>
+                            <input type="text" value="{{$item->nama_departemen}}" class="form-control" id="nama_departemen" name="nama_departemen_edit"
                                 aria-describedby="emailHelp">
+                            @error('nama_departemen_edit')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         <button type="submit" class="btn btn-primary tombol-aksi float-right">Simpan</button>
                         <button class="btn btn-danger tombol-aksi float-right" type="button" data-bs-dismiss="modal">Batal</button>
@@ -34,4 +43,20 @@
             </div>
         </div>
     </div>
+@endforeach
+
+<?php $listError = ['kode_departemen_edit', 'nama_departemen_edit']; ?>
+@foreach ($listError as $err)
+    @error($err)
+        <script type="text/javascript">
+             window.onload = function() {
+                OpenBootstrapPopup();
+            };
+
+            function OpenBootstrapPopup() {
+                $("#edit_departemen{{ $item->id_departemen }}").modal('show');
+            }
+        </script>
+    @break
+    @enderror
 @endforeach
