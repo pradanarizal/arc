5bc3d1209901fc0fc7b8e42ba4b0ba9fbb6959de
Rifky Yusuf Mahfuz <rifkyyusufmahfud@gmail.com>
2022-12-28 20:23:08 +0700

    fix order by

--- resources/views/superadmin/approval/pengarsipan.blade.php
+++ resources/views/superadmin/approval/pengarsipan.blade.php
@@ -11,29 +11,9 @@
     <div class="card shadow mb-4">
         <div class="card-body">
             <div class="table-responsive">
-                <div class="d-flex align-items-center justify-content-between mb-1 p-1">
-                    <div></div>
-                    <div></div>
-                    <div class="d-flex align-items-center justify-content-between">
-                        <form action="/approval/pengarsipan" method="GET"
-                            class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                            <div class="input-group">
-                                <input type="text" name="keyword" id="pengarsipanSearch"
-                                    class="form-control bg-light border-0 small" placeholder="Search" aria-label="Search"
-                                    aria-describedby="basic-addon2" value="{{ request('keyword') }}">
-                                <div class="input-group-append">
-                                    <button class="btn btn-primary" type="submit">
-                                        <i class="fas fa-search fa-sm"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <table class="table table-bordered" id="pengarsipanSearch" width="100%" cellspacing="0">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
-
                             <th>No</th>
                             <th>No. Dok</th>
                             <th>Nama Dokumen</th>
@@ -56,9 +36,9 @@
                                 <td>{{ $item->no_dokumen }}</td>
                                 <td>{{ $item->nama_dokumen }}</td>
                                 <td>{{ $item->jenis_dokumen }}</td>
-                                {{--<td>{{ $item->tahun_dokumen }}</td>--}}
+                                {{-- <td>{{ $item->tahun_dokumen }}</td> --}}
                                 <td>{{ $item->deskripsi }}</td>
-                                {{--<td>{{ date('d-m-Y', strtotime($item->tgl_upload)) }}</td>--}}
+                                {{-- <td>{{ date('d-m-Y', strtotime($item->tgl_upload)) }}</td> --}}
                                 <td>{{ $item->name }}</td>
                                 <td class="text-center">
                                     @if ($item->status_pengarsipan == 'Pending')
@@ -74,7 +54,8 @@
                                 </td>
                                 <td class="text-center">
                                     @if ($item->status_pengarsipan == 'Pending')
-                                        <button type="button" class="btn btn-sm bg-primary text-white" data-bs-toggle="dropdown">
+                                        <button type="button" class="btn btn-sm bg-primary text-white"
+                                            data-bs-toggle="dropdown">
                                             <i class="fas fa-cog"></i>
                                         </button>
                                         <ul class="dropdown-menu">
